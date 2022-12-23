7d760c1260d87671a2c53f0fc0dcf23452f47fe7
Marcos André <marcosdev2002@gmail.com>
2022-12-23 12:16:02 -0300

    Versão final

--- projetoAgenda/src/model/services/SearchContactsInArchives.java
+++ projetoAgenda/src/model/services/SearchContactsInArchives.java
@@ -1,43 +1,48 @@
 package model.services;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 import model.entities.Contact;
-import model.entities.ContactBook;
 
 public class SearchContactsInArchives implements ContactSearchService {
 	
 	@Override
-	public List<Contact> getContactList(ContactBook contactBook) {
-		try(Scanner readContacts = new Scanner(new BufferedReader(new FileReader(contactBook.getContactsFile())))){
+	public List<Contact> getContactList(File contactsFile) {
+		List<Contact> contactList = new ArrayList<>();
+
+		try(Scanner readContacts = new Scanner(new BufferedReader(new FileReader(contactsFile)))){
 			while(readContacts.hasNextLine()) {
 				String[] contactsString = readContacts.nextLine().split(",");
-				contactBook.getContactList().add(new Contact(contactsString[0], contactsString[1]));
+				contactList.add(new Contact(contactsString[0], contactsString[1]));
 			}
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage() + "Lista vazia!");
 			e.printStackTrace();
 		}
 
-		return contactBook.getContactList();
+		return contactList;
 	}
 	
 	@Override
-	public List<Contact> getDeletedContactsList(ContactBook contactBook) {
-		try(Scanner readContacts = new Scanner(new BufferedReader(new FileReader(contactBook.getContactsFile())))){
+	public List<Contact> getDeletedContactsList(File deletedContactsFile) {
+		List<Contact> deletedContactsList = new ArrayList<>();
+
+		try(Scanner readContacts = new Scanner(new BufferedReader(new FileReader(deletedContactsFile)))){
 			while(readContacts.hasNextLine()) {
 				String[] contactsString = readContacts.nextLine().split(",");
-				contactBook.getDeletedContactsList().add(new Contact(contactsString[0], contactsString[1]));
+				deletedContactsList.add(new Contact(contactsString[0], contactsString[1]));
 			}
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage() + "Lista vazia!");
 			e.printStackTrace();
 		}
 
-		return contactBook.getDeletedContactsList();
+		return deletedContactsList;
 	}
 }
